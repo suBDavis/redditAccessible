fb9c96f7c609041e3a28c5e7d046106ee3b61296
Brandon Davis <bdavis@redspin.net>
2017-04-22 17:52:02 -0400

    0.1.%

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # To be run in the same director in the same directory...
-zip -r ra.zip bg/ css/ img/ lib/ popup/ content_script.js manifest.json
+zip -r ra.zip bg/ css/ img/ lib/ popup/ ./content_script.js ./manifest.json
