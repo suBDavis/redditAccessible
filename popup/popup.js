163e5d2a84cdd3987621ea0e8a2cfbe262006192
Brandon Davis <brandon@subdavis.com>
2017-05-03 22:15:35 -0400

    Move images into images, add tutorial link

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
 const CHANGE_REDDIT_URL = "https://reddit.com/subreddits/mine";
+const TUTORIAL_URL = "https://subdavis.com/redditAccessible/#tutorial";
 
 function toggle(){
   var bttn = $("#toggle");
@@ -41,6 +42,9 @@ function setColor(bttn){
 function openMySubreddits(){
   chrome.tabs.create({url: CHANGE_REDDIT_URL});
 }
+function openTutorial(){
+  chrome.tabs.create({url:TUTORIAL_URL});
+}
 
 var hexDigits = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
 //Function to convert rgb color to hex format
@@ -72,6 +76,7 @@ window.onload = function () {
   $("#toggle").on('click', toggle);
   $("#toggleSpeech").on('click', toggleVoiceOver);
   $("#changeSubreddit").on('click', openMySubreddits);
+  $("#tutorial").on('click', openTutorial);
   $(".colorbutton").on('click', setColor);
 };
 
